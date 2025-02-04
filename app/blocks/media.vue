a355a46186acb5911115a25326b6b7567d87967b
Michael Doyle <hello@michaeldoyle.co>
2025-02-04 10:19:50 +0000

    Media sizes and placeholder class

--- app/blocks/media.vue
+++ app/blocks/media.vue
@@ -1,8 +1,8 @@
 <template>
 	<section>
 		<container :width="data.width">
-			<template v-if="data.image.id">
-				<nuxt-img :src="data.image.id" :alt="data.caption" sizes="100vw sm:50vw" :height="data.image.metadata.dimensions.height" :width="data.image.metadata.dimensions.width" :placeholder="data.image.metadata.lqip" :style="{ 'aspect-ratio': `${data.image.metadata.dimensions.width} / ${data.image.metadata.dimensions.height}` }" placeholder-class="placeholder" class="select-none" loading="lazy" />
+			<template v-if="data.image._id">
+				<nuxt-img :src="data.image._id" :alt="data.caption" sizes="100vw sm:100vw md:100vw lg:100vw xl:100vw" :height="data.image.metadata.dimensions.height" :width="data.image.metadata.dimensions.width" :placeholder="data.image.metadata.lqip" :style="{ 'aspect-ratio': `${data.image.metadata.dimensions.width} / ${data.image.metadata.dimensions.height}` }" placeholder-class="object-cover" class="select-none" loading="lazy" />
 			</template>
 		</container>
 	</section>
