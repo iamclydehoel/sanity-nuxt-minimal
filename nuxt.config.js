58d76113fd60a328821dc04c96858c7f4127f9a3
Michael Doyle <hello@michaeldoyle.co>
2025-02-11 14:43:01 +0000

    Add dataset fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
 
 	sanity: {
 		projectId: process.env.NUXT_SANITY_PROJECT_ID,
-		dataset: process.env.NUXT_SANITY_DATASET,
+		dataset: process.env.NUXT_SANITY_DATASET || 'production',
 		apiVersion: process.env.NUXT_SANITY_API_VERSION || '2025-01-31',
 		useCdn: false,
 	},
