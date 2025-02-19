d42efdd5134883ba2615afe81de2bd948323227f
Michael Doyle <hello@michaeldoyle.co>
2025-02-19 14:49:56 +0000

    Simplify import path

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
-import { schemaTypes } from '~~/sanity/schemas'
+import { schemaTypes } from './schemas'
 import './sanity.css'
 
 const config = useRuntimeConfig()
